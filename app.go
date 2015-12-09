5e256d9a0d36041d033d35f98379784bfa04fc03
Ondrej Odchazel <hypertornado@gmail.com>
2015-12-09 12:02:10 +0100

    recovery info should be available even in production mode

--- app.go
+++ app.go
@@ -100,11 +100,11 @@ func recoveryFromServerError(p Request, recoveryData interface{}) {
 	if p.App().DevelopmentMode() {
 		p.Response().Write([]byte(fmt.Sprintf("500 - error\n%s\nstack:\n", recoveryData)))
 		p.Response().Write(debug.Stack())
-		p.Log().Errorln(fmt.Sprintf("500 - error\n%s\nstack:\n", recoveryData))
-		p.Log().Errorln(string(debug.Stack()))
 	} else {
 		p.Response().Write([]byte("We are sorry, some error occured. (500)"))
 	}
+	p.Log().Errorln(fmt.Sprintf("500 - error\n%s\nstack:\n", recoveryData))
+	p.Log().Errorln(string(debug.Stack()))
 }
 
 func MiddlewareRemoveTrailingSlash(p Request) {
