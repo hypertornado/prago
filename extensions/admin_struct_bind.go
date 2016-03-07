17b0d40bb3a7ba644998f594d7a732d40187c689
Ondrej Odchazel <hypertornado@gmail.com>
2016-03-07 14:58:33 +0100

    refactoring

--- extensions/admin_struct_bind.go
+++ extensions/admin_struct_bind.go
@@ -19,8 +19,39 @@ func (cache *AdminStructCache) BindDataNEW(item interface{}, params url.Values, 
 	return nil
 }
 
-func (field *adminStructField) bindField(params url.Values, form *multipart.Form) {
+func (field *adminStructField) bindField(params url.Values, form *multipart.Form) (val reflect.Value) {
 
+	urlValue := params.Get(field.name)
+
+	switch field.typ.Kind() {
+	case reflect.Struct:
+		if field.typ == reflect.TypeOf(time.Now()) {
+			tm, err := time.Parse("2006-01-02", urlValue)
+			if err == nil {
+				val.Set(reflect.ValueOf(tm))
+			}
+		}
+	case reflect.String:
+		if field.tags["prago-admin-type"] == "image" {
+			imageId, err := NewImageFromMultipartForm(form, field.name)
+			if err == nil {
+				val.SetString(imageId)
+			}
+		} else {
+			val.SetString(urlValue)
+		}
+	case reflect.Bool:
+		if urlValue == "on" {
+			val.SetBool(true)
+		} else {
+			val.SetBool(false)
+		}
+	case reflect.Int64:
+		i, _ := strconv.Atoi(urlValue)
+		val.SetInt(int64(i))
+	}
+
+	return
 }
 
 func (cache *AdminStructCache) BindData(item interface{}, params url.Values, form *multipart.Form, bindDataFilter func(*adminStructField) bool) error {
@@ -38,8 +69,8 @@ func (cache *AdminStructCache) BindData(item interface{}, params url.Values, for
 		}
 
 		val := value.FieldByName(field.name)
-		urlValue := params.Get(field.name)
 
+		urlValue := params.Get(field.name)
 		switch field.typ.Kind() {
 		case reflect.Struct:
 			if field.typ == reflect.TypeOf(time.Now()) {
