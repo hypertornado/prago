ee66b383ab5703e22b83785c0287746440dedec1
Ondrej Odchazel <hypertornado@gmail.com>
2016-06-14 14:17:37 +0200

    betterdiagnostic when mysql is not running

--- extensions/mysql.go
+++ extensions/mysql.go
@@ -2,6 +2,7 @@ package extensions
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/hypertornado/prago"
@@ -15,10 +16,14 @@ func (m *Mysql) Init(app *prago.App) error {
 	password := app.Config().GetString("dbPassword")
 
 	connectString := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", user, password, dbName)
-
 	db, err := sql.Open("mysql", connectString)
 	if err != nil {
-		return err
+		return errors.New(fmt.Sprintf("Error while opening MySQL database: %s", err))
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error while opening MySQL database: %s", err))
 	}
 
 	app.Data()["db"] = db
