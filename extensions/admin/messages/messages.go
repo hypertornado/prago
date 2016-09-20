db15d7605f1763879f83db11ceb35a0634d4b6e9
Ondrej Odchazel <hypertornado@gmail.com>
2016-09-20 16:35:30 +0200

    use english as fallback locale

--- extensions/admin/messages/messages.go
+++ extensions/admin/messages/messages.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 )
 
-var Messages *messages
+var (
+	Messages         *messages
+	fallbackLanguage string = "en"
+)
 
 func init() {
 	m := map[string]map[string]string{
@@ -21,10 +24,12 @@ type messages struct {
 func (m *messages) Get(lang, id string, params ...interface{}) string {
 	ret := m.GetNullable(lang, id, params...)
 	if ret == nil {
-		return id
-	} else {
-		return *ret
+		ret = m.GetNullable(fallbackLanguage, id, params...)
 	}
+	if ret == nil {
+		return id
+	}
+	return *ret
 }
 
 func (m *messages) GetNullable(lang, id string, params ...interface{}) *string {
