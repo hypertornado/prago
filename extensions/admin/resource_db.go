2b1a62344d55e3c8d0bc2418a1cc1a82c6ded162
Ondrej Odchazel <hypertornado@gmail.com>
2016-09-28 16:45:31 +0200

    refactor db_tools

--- extensions/admin/resource_db.go
+++ extensions/admin/resource_db.go
@@ -1,64 +1,9 @@
 package admin
 
 import (
-	"errors"
 	"reflect"
 	"time"
 )
-
-//ErrWrongWhereFormat is returned when where query has a bad format
-var ErrWrongWhereFormat = errors.New("wrong where format")
-
-func (q *listQuery) where(data ...interface{}) error {
-	var whereParams []interface{}
-	var whereString string
-	var err error
-
-	if len(data) == 0 {
-		return ErrWrongWhereFormat
-	}
-	if len(data) == 1 {
-		whereString, whereParams, err = q.whereSingle(data[0])
-		if err != nil {
-			return err
-		}
-	} else {
-		first, ok := data[0].(string)
-		if !ok {
-			return ErrWrongWhereFormat
-		}
-		whereString = first
-		whereParams = data[1:len(data)]
-	}
-
-	if len(q.whereString) > 0 {
-		q.whereString += " and "
-	}
-	q.whereString += whereString
-	q.whereParams = append(q.whereParams, whereParams...)
-
-	return nil
-}
-
-func (q *listQuery) whereSingle(data interface{}) (whereString string, whereParams []interface{}, err error) {
-	switch data.(type) {
-	case string:
-		whereString = data.(string)
-	case int64:
-		whereString, whereParams = mapToDBQuery(map[string]interface{}{"id": data.(int64)})
-	case int:
-		whereString, whereParams = mapToDBQuery(map[string]interface{}{"id": data.(int)})
-	case map[string]interface{}:
-		whereString, whereParams = mapToDBQuery(data.(map[string]interface{}))
-	default:
-		err = ErrWrongWhereFormat
-	}
-	return
-}
-
-func (q *listQuery) addOrder(name string, desc bool) {
-	q.order = append(q.order, listQueryOrder{name: name, desc: desc})
-}
 
 func (ar *Resource) saveWithDBIface(item interface{}, db dbIface) error {
 	if !ar.HasModel {
