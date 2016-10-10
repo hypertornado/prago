2fc2b69953a8872e29dd6cec402a7ffaa76e0f2e
Ondrej Odchazel <hypertornado@gmail.com>
2016-10-10 14:26:21 +0200

    basic e2e test

--- e2e_test.go
+++ e2e_test.go
@@ -1,12 +1,37 @@
 package prago
 
 import (
-	//"github.com/hypertornado/prago"
+	"github.com/hypertornado/prago/selenium"
+	"net/http"
 	"testing"
+	"time"
+)
+
+var (
+	testServerRunning bool
+	driver            *selenium.Driver
 )
 
 func TestE2E(t *testing.T) {
+	prepareE2ETest(t)
 
+	session := driver.NewTestSession(t)
+	defer session.Delete()
+
+	session.SetURL("http://localhost:8587/h")
+	source := session.GetElementByID("hello").Text()
+
+	if source != "hello" {
+		t.Fatal(source)
+	}
+}
+
+func prepareE2ETest(t *testing.T) {
+	if testServerRunning {
+		return
+	}
+
+	driver = selenium.NewDriver("http://localhost:9515")
 	app := NewApp("prago", "1")
 	app.Config = &config{map[string]interface{}{}}
 
@@ -17,10 +42,28 @@ func TestE2E(t *testing.T) {
 	}
 	go app.ListenAndServe(8587, true)
 
+	for i := 0; i < 100; i++ {
+		time.Sleep(50 * time.Millisecond)
+		resp, _ := http.Get("http://localhost:8587/test")
+		if resp.StatusCode == 200 {
+			testServerRunning = true
+			return
+		}
+	}
+
 }
 
 func initE2Etest(app *App) {
-	app.MainController().Get("/", func(request Request) {
-		panic("eee")
+	app.MainController().Get("/h", func(request Request) {
+		request.Header().Add("Content-type", "text/html")
+		request.Response().WriteHeader(200)
+		request.Response().Write([]byte("<div id=\"hello\">hello</div>"))
+		request.SetProcessed()
+	})
+	app.MainController().Get("/*some", func(request Request) {
+		request.Header().Add("Content-type", "text/html")
+		request.Response().WriteHeader(200)
+		request.Response().Write([]byte(request.Params().Get("some")))
+		request.SetProcessed()
 	})
 }
