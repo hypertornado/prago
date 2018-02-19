0edb42d2c33091beaf9141828bbf3891e82508fb
Ondrej Odchazel <hypertornado@gmail.com>
2018-02-19 11:28:58 +0100

    better recovery with uuid

--- app.go
+++ app.go
@@ -2,11 +2,13 @@
 package prago
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"github.com/Sirupsen/logrus"
 	"github.com/hypertornado/prago/utils"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -23,7 +25,6 @@ type App struct {
 	DevelopmentMode    bool
 	Port               int
 	StartedAt          time.Time
-	RecoveryFunc       func(*Request, interface{})
 	Config             *config
 	data               map[string]interface{}
 	requestMiddlewares []requestMiddleware
@@ -41,7 +42,6 @@ type requestMiddleware func(Request, func())
 //NewApp creates App structure for prago app
 func NewApp(appName, version string) *App {
 	app := &App{
-		RecoveryFunc:       defaultRecovery,
 		data:               make(map[string]interface{}),
 		requestMiddlewares: []requestMiddleware{},
 		middlewares:        []Middleware{},
@@ -189,7 +189,7 @@ func (app *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	defer func() {
 		if recoveryData := recover(); recoveryData != nil {
-			app.RecoveryFunc(request, recoveryData)
+			recoveryFunction(request, recoveryData)
 		}
 	}()
 
@@ -208,7 +208,7 @@ func callRequestMiddlewares(request *Request, middlewares []requestMiddleware) {
 	f()
 }
 
-func defaultRecovery(p *Request, recoveryData interface{}) {
+func defaultRecoveryOLD(p *Request, recoveryData interface{}) {
 	uuid := utils.RandomString(10)
 
 	p.Response().WriteHeader(500)
@@ -216,3 +216,84 @@ func defaultRecovery(p *Request, recoveryData interface{}) {
 	p.Log().Errorln(fmt.Sprintf("500 Internal Server Error, errorid %s\n%s\nstack:\n", uuid, recoveryData))
 	p.Log().Errorln(string(debug.Stack()))
 }
+
+func recoveryFunction(p *Request, recoveryData interface{}) {
+	uuid := utils.RandomString(10)
+
+	if p.App().DevelopmentMode {
+		temp, err := template.New("development_error").Parse(recoveryTmpl)
+		if err != nil {
+			panic(err)
+		}
+		byteData := fmt.Sprintf("%s", recoveryData)
+
+		buf := new(bytes.Buffer)
+		err = temp.ExecuteTemplate(buf, "development_error", map[string]interface{}{
+			"name":    byteData,
+			"subname": fmt.Sprintf("500 Internal Server Error (errorid %s)", uuid),
+			"stack":   string(debug.Stack()),
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		p.Response().Header().Add("Content-type", "text/html")
+		p.Response().WriteHeader(500)
+		p.Response().Write(buf.Bytes())
+	} else {
+		p.Response().WriteHeader(500)
+		p.Response().Write([]byte(fmt.Sprintf("We are sorry, some error occured. (errorid %s)", uuid)))
+	}
+
+	p.Log().Errorln(fmt.Sprintf("500 - errorid %s\n%s\nstack:\n", uuid, recoveryData))
+	p.Log().Errorln(string(debug.Stack()))
+
+}
+
+const recoveryTmpl = `
+<html>
+<head>
+  <title>{{.subname}}: {{.name}}</title>
+
+  <style>
+  	html, body{
+  	  height: 100%;
+  	  font-family: Roboto, -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Segoe UI", Oxygen, Ubuntu, Cantarell, "Open Sans", sans-serif;
+  	  font-size: 15px;
+  	  line-height: 1.4em;
+  	  margin: 0px;
+  	  color: #333;
+  	}
+  	h1 {
+  		border-bottom: 1px solid #dd2e4f;
+  		background-color: #dd2e4f;
+  		color: white;
+  		padding: 10px 10px;
+  		margin: 0px;
+      line-height: 1.2em;
+  	}
+
+  	.err {
+  		font-size: 15px;
+  		margin-bottom: 5px;
+  	}
+
+  	pre {
+  		margin: 5px 10px;
+  	}
+
+  </style>
+
+</head>
+<body>
+
+<h1>
+	<div class="err">{{.subname}}</div>
+	{{.name}}
+</h1>
+
+<pre>{{.stack}}</pre>
+
+</body>
+</html>
+`
