eea632f8f5d12168e526427f9fb1a9be7a3fa341
Ondrej Odchazel <hypertornado@gmail.com>
2021-03-06 11:52:39 +0100

    refactor development

--- development.go
+++ development.go
@@ -1,8 +1,15 @@
 package prago
 
 import (
+	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"time"
+
+	"github.com/radovskyb/watcher"
 )
 
 type DevelopmentSettings struct {
@@ -28,7 +35,7 @@ func (app *App) InitDevelopment(settings DevelopmentSettings) {
 			func() {
 				app.DevelopmentMode = true
 				for _, v := range settings.Less {
-					go developmentLess(v.SourceDir, v.Target)
+					go app.developmentLess(v.SourceDir, v.Target)
 				}
 
 				for _, v := range settings.TypeScript {
@@ -47,4 +54,64 @@ func developmentTypescript(path string) {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Start()
+}
+
+func (app *App) fileWatcher(path string, handler func()) {
+	w := watcher.New()
+	w.SetMaxEvents(1)
+
+	go func() {
+		for {
+			select {
+			case event := <-w.Event:
+				fmt.Println(event) // Print the event's info.
+				handler()
+			case err := <-w.Error:
+				log.Fatalln(err)
+			case <-w.Closed:
+				return
+			}
+		}
+	}()
+
+	if err := w.AddRecursive(path); err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := w.Start(time.Millisecond * 100); err != nil {
+		panic(err)
+	}
+}
+
+func (app *App) developmentLess(sourcePath, targetPath string) {
+	compileLess(filepath.Join(sourcePath, "index.less"), targetPath)
+	app.fileWatcher(sourcePath, func() {
+		compileLess(filepath.Join(sourcePath, "index.less"), targetPath)
+	})
+}
+
+func compileLess(from, to string) error {
+	outfile, err := os.Create(to)
+	if err != nil {
+		return err
+	}
+	defer outfile.Close()
+
+	return commandHelper(exec.Command("lessc", from), outfile)
+}
+
+func commandHelper(cmd *exec.Cmd, out io.Writer) error {
+	var err error
+	cmd.Stdout = out
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- development_linux.go
+++
@@ -1,5 +0,0 @@
-package prago
-
-func developmentLess(sourcePath, targetPath string) {
-	panic("Less development not implemented for linux")
-}
