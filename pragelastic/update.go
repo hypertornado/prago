5a2a5f390ae197100c358d7f51845cba0dbc109f
Ondrej Odchazel <hypertornado@gmail.com>
2023-09-17 17:48:28 +0200

    rewrite to use new bulk api

--- pragelastic/update.go
+++ pragelastic/update.go
@@ -6,12 +6,15 @@ import (
 	"strings"
 
 	"github.com/elastic/go-elasticsearch/v7/esapi"
+	"github.com/elastic/go-elasticsearch/v7/esutil"
 	"github.com/olivere/elastic/v7"
 )
 
 type BulkUpdater[T any] struct {
 	index     *Index[T]
 	processor *elastic.BulkProcessor
+
+	indexer esutil.BulkIndexer
 }
 
 func (index *Index[T]) UpdateSingleNew(item *T) error {
@@ -35,61 +38,47 @@ func (index *Index[T]) UpdateSingle(item *T) error {
 		request.DocumentID = id
 	})
 	return err
-
-	/*_, err := index.
-	client.
-	esclientOld.
-	Index().
-	Index(
-		index.indexName(),
-	).
-	BodyJson(item).
-	Id(id).
-	Do(
-		context.Background(),
-	)*/
-	//return err
 }
 
 func (index *Index[T]) UpdateBulk() (*BulkUpdater[T], error) {
-	bp, err := index.client.esclientOld.BulkProcessor().Name("prago-bulk-updater").
-		Before(func(executionId int64, requests []elastic.BulkableRequest) {
-		}).
-		After(func(executionId int64, requests []elastic.BulkableRequest, response *elastic.BulkResponse, err error) {
-			/*for _, v := range response.Failed() {
-				fmt.Println(v.Error)
-			}*/
-		}).
-		Workers(2).Do(context.Background())
-	if err != nil {
-		return nil, err
-	}
-	err = bp.Start(context.Background())
-	if err != nil {
-		return nil, err
-	}
+
+	indexer, _ := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+		Index:      index.indexName(),
+		ErrorTrace: true,
+		OnError: func(ctx context.Context, err error) {
+			panic(err)
+		},
+	})
 
 	ret := &BulkUpdater[T]{
-		index:     index,
-		processor: bp,
+		index:   index,
+		indexer: indexer,
 	}
+
 	return ret, nil
 }
 
 func (updater *BulkUpdater[T]) AddItem(item *T) {
 	id := getID(item)
-	r := elastic.NewBulkIndexRequest()
-	r.
-		Index(updater.index.indexName()).
-		Id(id).
-		Doc(item)
-	updater.processor.Add(r)
+
+	data, err := json.Marshal(item)
+	if err != nil {
+		panic(err)
+	}
+
+	err = updater.indexer.Add(
+		context.Background(),
+		esutil.BulkIndexerItem{
+			DocumentID: id,
+			Action:     "index",
+			Body:       strings.NewReader(string(data)),
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func (updater *BulkUpdater[T]) Close() error {
-	err := updater.processor.Flush()
-	if err != nil {
-		return err
-	}
-	return updater.processor.Close()
+	return updater.indexer.Close(context.Background())
 }
